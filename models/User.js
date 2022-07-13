9fdab91200fea656c89c80998ad84e0725bf8403
dvrakry <dv.rakry@gmail.com>
2022-07-13 11:06:42 +0900

    2022-07-13

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -9,6 +11,10 @@ const userSchema = mongoose.Schema({
     type: String,
     trim: true,
     unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
   },
   lastname: {
     type: String,
@@ -25,6 +31,28 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+//pre('save') -> user 정보를 저장하기 전에 라는 뜻!
+userSchema.pre("save", function (next) {
+  //원래 입력한 비밀번호 가져오기
+  var user = this;
+
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        //hash 가 새로 생성된 비밀번호임
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
